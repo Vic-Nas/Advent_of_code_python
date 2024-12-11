fa675ab1f08dc75536026cfc6d03399635293eab
Vic-Nas <155357371+Vic-Nas@users.noreply.github.com>
2024-12-10 22:11:52 -0500

    Update 01_2024.py

--- 2024/01_2024.py
+++ 2024/01_2024.py
@@ -12,4 +12,5 @@ l2.sort()
 
 res1 = sum(abs(l1[i] - l2[i]) for i in range(len(l1)))
 res2 = sum(el * l2.count(el) for el in l1)
-print(f"\nPart1: {res1}\nPart2: {res2}\n")
+print(f"\nPart1: {res1}")
+print(f"Part2: {res2}\n")
